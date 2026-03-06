36eb1af178e6aaa32591a48f5cf5f839a0036464
Bloedrenn <bloedrenn@gmail.com>
2026-03-06 21:04:07 +0300

    E: подключил Header в App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
+import Header from './components/UI/Header/Header.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1>Счётчик кликов</h1>
       <button onClick={() => setCount(count + 1)}>
         Клик: {count}
       </button>
+      <Header /> {/* Для примера */}
     </>
   )
 }
