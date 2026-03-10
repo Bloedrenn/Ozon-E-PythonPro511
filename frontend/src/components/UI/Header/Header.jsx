caad21e187fad868a37de181afeddd6e735e10bd
Bloedrenn <bloedrenn@gmail.com>
2026-03-11 00:48:52 +0300

    E: создал css-модуль для Header'а

--- frontend/src/components/UI/Header/Header.jsx
+++ frontend/src/components/UI/Header/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.css'
+import styles from './Header.module.css'
 
 
 const Header = ({ text: myText, title = "Заголовок" }) => (
-    <header className="header">
+    <header className={styles.header}>
         <p>{myText}</p>
         <p>{title}</p>
     </header>
