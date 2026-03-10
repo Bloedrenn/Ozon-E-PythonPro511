bf4f15228b71ed242e1be2de3848f03087a70e24
Bloedrenn <bloedrenn@gmail.com>
2026-03-10 18:23:49 +0300

    E: передача функции по ссылке в качестве callback, событие onMouseEnter (для примера)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,19 @@ import Header from '@components/UI/Header'
 function App() {
   const [count, setCount] = useState(0)
 
+  const pHover = () => { 
+    console.log("Навели на p")
+    console.log("Я БОЛЬШАЯ ФУНКЦИЯ")
+    console.log("Я БОЛЬШАЯ ФУНКЦИЯ")
+    console.log("Я БОЛЬШАЯ ФУНКЦИЯ")
+    console.log("ВО МНЕ МНОГО СТРОЧЕК КОДА")
+    console.log("ВО МНЕ МНОГО СТРОЧЕК КОДА")
+  }
+
   return (
     <>
       <Header />
+      <p onMouseEnter={pHover}>Hello World</p>
       <h1>Счётчик кликов</h1>
       <button onClick={() => setCount(count + 1)}>
         Клик: {count}
