5fd1c85531322116e81b968ad454b30500eed7d0
Bloedrenn <bloedrenn@gmail.com>
2026-03-17 19:03:58 +0300

    E: вынес мусор

--- frontend/src/components/UI/Header/Header.jsx
+++ frontend/src/components/UI/Header/Header.jsx
@@ -1,26 +1,8 @@
-import { useEffect } from 'react'
-
 import styles from './Header.module.css'
 
 
-const Header = ({ text: myText, title = "Заголовок" }) => {
-    useEffect(
-        () => { // можно убрать {}
-            return () => console.log("Header демонтировался") // Функция очистки (cleanup)
-        },
-        []
-        // Важно указать []. Иначе получим 2 лога в консоли.
-        // Эффект выполняется только один раз при монтировании компонента.
-        // При последующих рендерах (rerender) эффект НЕ запускается снова, поэтому cleanup не вызывается.
-        // Cleanup выполнится только один раз — при размонтировании компонента.
-    )
-
-    return (
-        <header className={styles.header}>
-            <p>{myText}</p>
-            <p>{title}</p>
-        </header>
-    )
+const Header = () => {
+    return <header className={styles.header}>Шапка сайта</header>
 }
 
 export default Header
