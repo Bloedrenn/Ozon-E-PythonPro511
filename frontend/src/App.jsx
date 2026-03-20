6615a217bf363344cf5138955f37ac4ad58bd245
Bloedrenn <bloedrenn@gmail.com>
2026-03-20 20:23:57 +0300

    feat(ItemList): Создать компонент ItemList (с данными в самом компоненте) и подключить в App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,13 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
 import Header from '@components/UI/Header'
-
-const API_URL = import.meta.env.VITE_API_URL
+import ItemList from '@components/Items/ItemList'
 
 
 function App() {
-  // items = [
-  //   {id: "1", name: "Xiaomi Redmi 10", description: "Топ за свои деньги!", isAvailable: true},
-  //   {id: "2", name: "Iphone X", description: "Крутой телефон (нет) и дорогой (да)", isAvailable: false}
-  // ]
-  const [items, setItems] = useState([])
-  const [itemsLoading, setItemsLoading] = useState(true)
-  const [itemsError, setItemsError] = useState(null)
-
-  useEffect(() => {
-    axios.get(`${API_URL}/items`)
-      .then(response => {
-        setItems(response.data)
-        setItemsLoading(false)
-      })
-      .catch(error => {
-        console.error(error)
-        setItemsLoading(false)
-
-        const errMsg = (
-          error.message === "Network Error" ? "Ошибка сети"
-          : error.message === "Request failed with status code 404" ? "Ресурс не найден" // Можно так: error.response.status === 404
-          : "Повторите попытку позже"
-        )
-        setItemsError(`Ошибка загрузки вещей: ${errMsg}`)
-      })
-  }, [])
-
   return (
     <>
       <Header />
 
-      <h1>Список вещей</h1>
-      <ul>
-        {
-          itemsLoading ? (
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Загрузка вещей...</span>
-            </div>
-          ) : itemsError ? (
-            <div>{itemsError}</div>
-          ) : (
-            items.map(item => (
-              <li key={item.id}>
-                <strong>{item.name}</strong>: {item.description} - {item.isAvailable ? "В наличии" : "Нет в наличии"}
-              </li>
-            ))
-          )
-        }
-      </ul>
+      <ItemList />
     </>
   )
 }
